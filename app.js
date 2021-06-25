61a88960a650c26da004b6832b05ef7f63b0036d
TraceRicholson <trace.richolson@spaceforce.mil>
2021-06-25 09:52:01 -0500

    add GET 404 error handling

--- app.js
+++ app.js
@@ -41,9 +41,14 @@ app.get('/movies', (req, res) => res.send(movies2))
 // return movie by specific ID
 app.get('/movies/:id', (req, res) => {
   //404 ERROR
+  if (req.params.id-1 === NaN){
+    res.status(400).send("Please supply a number for the movie ID. :)")
+  }
   if (movies2[req.params.id-1] === undefined){
     res.status(404).send("NOT FOUND")
   }
+
+
   //RETURN STATEMENT
   console.log(req.params)
   console.log(req.params.id-1)
